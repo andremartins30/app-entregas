027df60b95cf82cb8f470e696c19929f32a92678
Andre Martins <an.martins30@gmail.com>
2025-03-27 21:53:32 -0400

    adiciona tela inicial e tela de passageiros; atualiza App.js

---
+++ src/screens/Home/home.jsx
@@ -0,0 +1,13 @@
+import { Text } from "react-native"
+
+export const Home = () => {
+    return (
+        <>
+        <Text style={{ fontSize: 50, color: "#fffb00", textAlign: "center", marginTop: 20, fontWeight: "bold",}}>Logotipo</Text>
+        <Text style={{ fontSize: 40, color: "#fffb00", textAlign: "center", marginTop: 40, fontWeight: "bold",}}>Vendedores</Text>
+        <Text style={{ fontSize: 40, color: "#fffb00", textAlign: "center", marginTop: 20, fontWeight: "bold",}}>Motoristas</Text>
+        </>
+    )
+}
+
+export default Home
---
+++ src/screens/Passengers/Passengers.jsx
@@ -0,0 +1,14 @@
+import { Text } from "react-native"
+
+const Passengers = () => {
+    return (
+        <>
+            <Text>Tela Passenger</Text>
+            <Text>Mapa</Text>
+            <Text>Origem/Destino</Text>
+        </>
+
+    )
+}
+
+export default Passengers
